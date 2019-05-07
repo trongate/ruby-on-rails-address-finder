2cb3c692fc14b94664a8de554e3ae6f44e532155
David Connelly <david.webguy@gmail.com>
2019-05-07 16:47:01 +0100

    UPdated to do list.

---
+++ _junk/todo.php
@@ -0,0 +1,34 @@
+WEDNESDAY
+
+1.  Get pagination and search working for donor module.
+
+2.  Test (and Fix?) all of the API endpoints.
+
+THURSDAY -> FRIDAY
+
+1.  Get desktop app automatically generating module.
+
+SATURDAY
+
+1.  Download auto-generated module to target location.
+
+SUNDAY
+
+----
+
+MONDAY to WEDNESDAY
+
+1.  Get Module relations working.
+
+
+THURSDAY to FRIDAY
+
+1.  Get image uploaders working.
+
+MONDAY to FRIDAY
+
+1.  Test test test then LAUNCH!
+
+*********************************
+*********************************
+*********************************
