244047f8f39e769429732b80ea12ac45f58e5aad
David Connelly <david.webguy@gmail.com>
2019-05-13 12:44:06 +0100

    Cleaned up donor module.  Ready to integrate with code generator.

--- _junk/todo.php
+++ _junk/todo.php
@@ -1,31 +1,6 @@
-FIXES
+MONDAY
 
-* Get records on /manage starting from 0 index.                                 DONE.
-
-* Test the pagination thoroughly REALLY MAKE SURE IT WORKS!!!!!!!               DONE.
-
-* Do not show pagination unless required.                                       DONE.
-
-* Get showing statement working.                                                DONE.
-
-* Add pagination to page btm (but without showing statement).                   DONE.
-
-* get 'per page' working.                                                       DONE.
-
-* Add 'no comments yet' message when no comments.                               DONE.
-
-* Retain memory of search phrase and make sure works with 'per page' feature.   DONE.
-
-* Have it gracefully saying 'no results' message when search is not successful. DONE.
-
-* Check ALL endpoints on API Explorer (one by one) and make sure they are cool. DONE.
-
---------------------------------------------------------------------------------
-
-BONUS FIXES
- 
-
-* Have redirect respecting custom routing.
+* javascript tutorial.
 
 * clean up code.
 
@@ -33,10 +8,10 @@ BONUS FIXES
 
 * Merge with the main framework branch
 
+* Have redirect() respecting custom routing.
+
 --------------------------------------------------------------------------------
 
-MONDAY
-
-* Upload this to Speed Coding Academy as 1.4.1
+TUESDAY  until the end of the week....
 
 * Integrate with code generator!!!!!!!
